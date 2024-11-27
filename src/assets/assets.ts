e8266779ae82e2d50734cbb07f3d1114e0bcf0cb
sefyan0hack <sefyanhack@gmail.com>
2024-11-27 03:00:27 +0100

    change img and decription

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -2,10 +2,12 @@ import Product_img from './a.jpg'
 import Product_lahsa from './lahsa.png'
 import navbar from './navbar.png'
 import Whatsappico from './whatsapp.ico'
+import Tahamil from './Hahamil.png'
 
 export const assets = {
     Product_img,
     Product_lahsa,
     Whatsappico,
-    navbar
+    navbar,
+    Tahamil
 }
